eea222e92d7874f1a30db702908146f5d777cb69
Sharon Olivia <165590862+SharonOliv@users.noreply.github.com>
2025-02-04 01:04:34 +0530

    server.py

--- server.py
+++ server.py
@@ -1,35 +1,24 @@
 from socket import *
 
 def main():
-    server_ip = '10.1.95.93'  # Replace with your server's IP address
+    server_ip = '10.1.95.93'  #server's IP address
     server_port = 21312
-
     server_socket = socket(AF_INET, SOCK_STREAM)
     server_socket.bind((server_ip, server_port))
     server_socket.listen(1)
-
     print(f"Server started on {server_ip}:{server_port}. Waiting for connections...")
-    
     try:
         conn, addr = server_socket.accept()
         print(f"Connection established with {addr}")
-
         while True:
-            # Receive message from the client
             message = conn.recv(1024).decode()
             if not message or message.lower() == 'bye':
                 print("Client disconnected.")
                 break
 
-            # Display received message
             print(f"CLIENT SENT: {message}")
-
-            # Get server's response
             server_message = input("SERVER: ")
-
-            # Send the server's response
             conn.send(server_message.encode())
-
             if server_message.lower() == "bye":
                 print("Closing connection...")
                 break
